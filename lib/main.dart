b8135a5fe37da6970c5010c358cf58d9610767bf
Romany Raafat <romanyraafat96@gmail.com>
2025-12-26 18:13:07 +0200

    fake commit 3

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello Flutter'),
+        ),
+        body: const Center(
+          child: Text('Welcome to Flutter!'),
+        ),
+      ),
   );
   }
 }
